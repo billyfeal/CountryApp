d3c7eaa2a7a9935b3f8c46e216224ce8a380dd4e
wfealdelgado <wfealdelgado@hedgeserv.com>
2025-08-15 09:05:27 -0400

    Adding files

---
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { CountryService } from '../../services/country.service';
+
+@Component({
+  selector: 'app-by-capital-page',
+  imports: [SearchInputComponent, CountryListComponent],
+  templateUrl: './by-capital-page.component.html',
+})
+export class ByCapitalPageComponent {
+  countryService = inject(CountryService);
+
+  onSearch(searchQuery: string) {
+    this.countryService.searchByCapital(searchQuery).subscribe((res) => {
+      console.log(res);
+    });
+  }
+}
